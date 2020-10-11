8687bf1f72a7d25dc1651c688c9ce6d1ba5ee48e
Igor Plyukhin <51264207+igorplyukhin@users.noreply.github.com>
2020-10-11 19:28:36 +0500

    dev

--- main.py
+++ main.py
@@ -5,9 +5,14 @@ import logging
 import time
 from datetime import datetime
 from cache import cache_request, _cached_requests
+import multiprocessing
+import subprocess
+import requests
 
 time_last_writen_to_db = 11
 WRITE_TO_DB_FREQ = 10
+HOST = '127.0.0.1'
+PORT = 8080
 
 
 @atomic
@@ -26,9 +31,27 @@ async def handle(request):
     return web.Response(body=body, content_type=mimetypes.guess_type(request.path)[0])
 
 
-if __name__ == '__main__':
+async def proxy_to_bd_server(request):
+    r = requests.get(url=f'http://127.0.0.1:5000{request.path_qs}')
+    content_type = r.headers['Content-Type'].split(';')[0]  # trim charset utf-8
+    return web.Response(body=r.text, content_type=content_type)
+
+
+def run_statistic_server():
+    subprocess.Popen(['sqlite_web', '-p', '5000', '-u', '/stats', '--no-browser', '--read-only',
+                      'hits_counter.sqlite'])
+
+
+def run_custom_server():
     app = web.Application()
     app.add_routes([web.get('/', handle),
+                    web.get(r'/{statistic:stats.*}', proxy_to_bd_server),
                     web.get('/{name}', handle)])
     setup(app)
-    web.run_app(app, host='192.168.1.215', port=8080)
+    web.run_app(app, host=HOST, port=PORT)
+
+
+if __name__ == '__main__':
+    p = multiprocessing.Process(target=run_statistic_server())
+    p.start()
+    run_custom_server()
